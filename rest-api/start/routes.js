e20d58b4fa1b67d6207f9bd1f14d3bb3fd30f3d8
David Portador <david.raw172@gmail.com>
2022-05-23 01:55:30 -0500

    Método Create

--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -23,5 +23,6 @@ Route.get('/', () => {
 Route.group(()=>{
   Route.post('usuarios/registro', 'UserController.store');
   Route.post('usuarios/login', 'UserController.login');
-  Route.get('proyectos', 'ProyectoController.index');
+  Route.get('proyectos', 'ProyectoController.index').middleware('auth');
+  Route.post('proyectos', 'ProyectoController.create').middleware('auth');
 }).prefix('api/v1/');
