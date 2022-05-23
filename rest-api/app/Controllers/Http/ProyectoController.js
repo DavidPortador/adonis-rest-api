cd543b0571e5a25a26ddb1f46a78481293ae69f5
David Portador <david.raw172@gmail.com>
2022-05-23 02:24:43 -0500

    Método Destroy

--- rest-api/app/Controllers/Http/ProyectoController.js
+++ rest-api/app/Controllers/Http/ProyectoController.js
@@ -19,6 +19,19 @@ class ProyectoController {
         await user.proyectos().save(proyecto);
         return proyecto;
     }
+
+    async destroy({ auth, response, params }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        if (proyecto.user_id !== user.id){
+            return response.status(403).json({
+                mensaje: "Error 403: No puedes eliminar el registro porque no eres dueño"
+            })
+        }
+        await proyecto.delete();
+        return proyecto;
+    }
 }
 
 module.exports = ProyectoController
